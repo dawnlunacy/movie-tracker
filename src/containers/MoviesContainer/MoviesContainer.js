c0434465a64d7e394f5178cb2f5d333999e20195
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-22 19:04:32 -0600

    Link up moviescontainer component

---
+++ src/containers/MoviesContainer/MoviesContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import './MoviesContainer.css'
+
+const MoviesContainer = () => {
+  return (
+    <main>
+      Testing
+    </main>
+  )
+}
+
+export default MoviesContainer;
