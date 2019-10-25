b13bd2cb8fe5953e41b738d3192c3758a0fa2a16
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-25 17:58:32 -0600

    Add tests for rest of handle change

--- src/containers/SignUpForm/SignUpForm.test.js
+++ src/containers/SignUpForm/SignUpForm.test.js
@@ -21,10 +21,48 @@ describe('SignUpForm', () => {
       }
     }
 
-    // wrapper.instance().handleChange = jest.fn();
     wrapper.instance().handleChange(mockEvent)
 
     expect(wrapper.state('name')).toEqual('Fernando')
-  })
+  });
 
-})
+  it('should update name in state when handleChange is called', () => {
+    const mockEvent = {
+      target: {
+        name: 'email',
+        value: 'crazy_cats@hotmail.com'
+      }
+    }
+
+    wrapper.instance().handleChange(mockEvent)
+
+    expect(wrapper.state('email')).toEqual('crazy_cats@hotmail.com')
+  });
+
+  it('should update name in state when handleChange is called', () => {
+    const mockEvent = {
+      target: {
+        name: 'password',
+        value: 'shhh'
+      }
+    }
+
+    wrapper.instance().handleChange(mockEvent)
+
+    expect(wrapper.state('password')).toEqual('shhh')
+  });
+
+  it('should update name in state when handleChange is called', () => {
+    const mockEvent = {
+      target: {
+        name: 'id',
+        value: 10
+      }
+    }
+
+    wrapper.instance().handleChange(mockEvent)
+
+    expect(wrapper.state('id')).toEqual(10)
+  });
+
+});
