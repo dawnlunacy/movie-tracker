4d7abf4f57ea6dd19f0e7cdc8cb1d028c7ad1445
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-24 22:34:19 -0600

    Write snapshot for moviescontainer and try to fix app failing tests

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,7 @@ class App extends Component {
       const cleanMovies = filteredMovieData(movies.results)
       isLoading(false)
       getMovies(cleanMovies)
+      console.log(getMovies(cleanMovies))
     } catch {
       isLoading(false)
       handleError('There was an error getting your movies!')
@@ -34,13 +35,13 @@ class App extends Component {
     return (
         <div className="App">
         <Route exact path='/login' render={ () => <UserForm /> } />
-        
-        <Route exact path='/' render={ () => 
+
+        <Route exact path='/' render={ () =>
           <>
           <header className="App-header">
             <Nav />
             <img src={logo} alt="Logo" className="App-img"/>
-          </header> 
+          </header>
             <MoviesContainer errorMessage={errorMessage} movies={movies} />
         </>
         } />
