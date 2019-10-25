8a887c5664157785282a8f846204a58d5d7d30a3
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-25 17:56:11 -0600

    Add test for handle name change

--- src/containers/SignUpForm/SignUpForm.test.js
+++ src/containers/SignUpForm/SignUpForm.test.js
@@ -3,11 +3,28 @@ import SignUpForm from './SignUpForm';
 import { shallow } from 'enzyme';
 
 describe('SignUpForm', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<SignUpForm/>); 
+  });
 
   it('should match snapshot', () => {
-    const wrapper = shallow(<SignUpForm/>);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update name in state when handleChange is called', () => {
+    const mockEvent = {
+      target: {
+        name: 'name',
+        value: 'Fernando'
+      }
+    }
+
+    // wrapper.instance().handleChange = jest.fn();
+    wrapper.instance().handleChange(mockEvent)
+
+    expect(wrapper.state('name')).toEqual('Fernando')
   })
 
 })
