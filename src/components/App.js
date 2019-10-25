74a2dad985e9c63f622518054e5844feed658bbd
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-25 16:06:09 -0600

    Add snapshot for App

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ import { filteredMovieData } from '../utils/helpers';
 import './App.css';
 import logo from '../images/MovieTracker_font_wave.png';
 
-class App extends Component {
+export class App extends Component {
 
   async componentDidMount() {
     const { getMovies, handleError, isLoading } = this.props
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import { App, mapStateToProps, mapDispatchToProps } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('should match snapshot', () => {
+    const wrapper = shallow (<App />)
+
+    expect(wrapper).toMatchSnapshot()
+  });
+
+}); 
 
