b59173cbda6cf6b47a3fe13718599c3ab9a535a9
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-26 22:52:22 -0600

    Render favorites link and message and edit link and link hover colors

--- src/containers/Nav/Nav.js
+++ src/containers/Nav/Nav.js
@@ -6,10 +6,11 @@ import './Nav.css'
 export const Nav = ({ currentUser }) => {
     return(
         <nav className="nav">
-            <p className="nav-p">FAVORITES</p>
+            {currentUser !== null && <Link className="link" to='/favorites'> <p className="nav-p">FAVORITES</p> </Link>}
+            {currentUser === null && <p className="nav-p">LOGIN TO VIEW FAVORITES</p> }
             <div className="nav-user-options">
-            {currentUser !== null && <Link className="link" to='/'> <p className="nav-p">LOGOUT</p> </Link>}
             {currentUser !== null && <p className="nav-p">Hello, {currentUser.name}</p>}
+            {currentUser !== null && <Link className="link" to='/'> <p className="nav-p">LOGOUT</p> </Link>}
             {currentUser === null && <Link className="link" to='/login'> <p className="nav-p">LOGIN</p> </Link>}
             {currentUser === null && <Link className="link" to='/signup'> <p className="nav-p">SIGN UP</p> </Link>}
             </div>
