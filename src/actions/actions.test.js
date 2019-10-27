8bbd8ef89da4ea314b2837e9aa29ecfe6da821b9
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-26 21:14:03 -0600

    Add getMovies redux test

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,7 +1,46 @@
 import * as actions from '../actions';
 
 describe('actions', () => {
-  it('should have a type of ADD_TODO', () => {
-    expect(true).toEqual(false);
+  it('should have a type of GET_MOVIES', () => {
+    const movies = [
+      {
+        id: 475557,
+        overview: "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
+        poster: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+        rating: 8.6,
+        title: "Joker"
+      },
+      {
+        id: 474350,
+        overview: "27 years after overcoming the malevolent supernatural entity Pennywise, the former members of the Losers' Club, who have grown up and moved away from Derry, are brought back together by a devastating phone call.",
+        poster: "/zfE0R94v1E8cuKAerbskfD3VfUt.jpg",
+        rating: 6.9,
+        title: "It Chapter Two"
+      }
+    ]
+    const expectedAction = {
+      type: 'GET_MOVIES',
+      movies: [
+        {
+          id: 475557,
+          overview: "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
+          poster: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+          rating: 8.6,
+          title: "Joker"
+        },
+        {
+          id: 474350,
+          overview: "27 years after overcoming the malevolent supernatural entity Pennywise, the former members of the Losers' Club, who have grown up and moved away from Derry, are brought back together by a devastating phone call.",
+          poster: "/zfE0R94v1E8cuKAerbskfD3VfUt.jpg",
+          rating: 6.9,
+          title: "It Chapter Two"
+        }
+      ]
+    }
+
+    const result = actions.getMovies(movies);
+    
+    expect(result).toEqual(expectedAction);
   });
+
 });
