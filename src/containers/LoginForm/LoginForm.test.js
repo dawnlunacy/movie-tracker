beb6b7c50878333c509e60b84becf09f365aa249
dawnlunacy <rudd.lacy@gmail.com>
2019-10-27 14:48:25 -0600

    Add some snapshots tests for handleChange

--- src/containers/LoginForm/LoginForm.test.js
+++ src/containers/LoginForm/LoginForm.test.js
@@ -1,13 +1,81 @@
 import React from 'react';
 import { LoginForm } from './LoginForm';
 import { shallow } from 'enzyme';
+// import { saveUser } from '../../actions/index';
 
-describe('LoginForm', () => {
 
-  it('should match snapshot', () => {
-    const wrapper = shallow(<LoginForm />);
+describe('LoginFormContainer', () => {
+  describe('AddTodoForm component', () => {
+    let wrapper;
+    // mockSaveUser = jest.fn();
 
-    expect(wrapper).toMatchSnapshot();
+    beforeEach(() => {
+      wrapper = shallow(<LoginForm />)
+    })
+
+    it('should match snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    })
+    
+    it('should set state of UserInput email when handleChange is called with an event', () => {
+      const emailEvent = {
+        target: {
+          name: "email",
+          value: "rudd.lacy@gmail.com"
+        }
+      }
+     
+      wrapper.instance().handleChange(emailEvent);
+
+      expect(wrapper.state('userInput').email).toEqual(emailEvent.target.value);
+      })
+
+    it('should set state of UserInput password when handleChange is called with an event', () => {
+
+      const passwordEvent = {
+        target: {
+         name: "password",
+         value: "password"
+        }
+      }
+
+    wrapper.instance().handleChange(passwordEvent);
+
+    expect(wrapper.state('userInput').password).toEqual(passwordEvent.target.value);
+    })
+
+    it('should reset state of errors when handleChange is called', () => {
+      const defaultState = {
+        userInput: {
+            email: 'mooses@forest.io',
+            password: 'MooseIsBetterThanPants',
+        },
+        error: 'Username or password incorrect',
+        formReady: false,
+        isLoggedIn: false
+      }
+      const expected = {
+        userInput: {
+            email: 'mooses@forest.io',
+            password: 'MoosesAreWAYBetterThanPants',
+        },
+        error: '',
+        formReady: false,
+        isLoggedIn: false
+      }
+
+      const passwordEvent = {
+        target: {
+         name: "password",
+         value: "MoosesAreWAYBetterThanPants"
+        }
+      }
+
+      wrapper.instance().setState(defaultState);
+
+      wrapper.instance().handleChange(passwordEvent);
+
+      expect(wrapper.state()).toEqual(expected);
+    })
   })
-
 })
