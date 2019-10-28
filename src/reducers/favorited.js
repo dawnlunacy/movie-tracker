f6c8b3c313e13aa40247903e8eadc21524fdbf21
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-28 14:27:16 -0600

    Allow for new movies to be added to the same favorited array in store

--- src/reducers/favorited.js
+++ src/reducers/favorited.js
@@ -2,6 +2,8 @@ export const favorited = (state = null, action) => {
   switch (action.type) {
     case 'RETRIEVE_FAVORITED':
       return action.favorited
+    case 'SAVE_NEW_FAVORITE':
+      return [...state, action.favorited]
     default:
       return state
   }
