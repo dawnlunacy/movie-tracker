cb46ab160e45265d62362f98557d52510e63fc55
dawnlunacy <rudd.lacy@gmail.com>
2019-10-27 18:50:39 -0600

    Clean up spacing in apiCalls and move some tests that had been started in apiCalls for App into App.test.js

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -12,3 +12,32 @@ describe('App', () => {
 
 }); 
 
+
+
+
+
+// describe('App', () => {
+//   const mockResponse = [
+//     {
+//       id: 4444,
+//       overview: 'puppies puppies puppies',
+//       poster: 'poster path goes here',
+//       rating: 9.9,
+//       title: 'Best in show' 
+//     }
+//   ]
+
+//   beforeEach(() => {
+//     window.fetch = jest.fn().mockImplementation(() => {
+//       return Promise.resolve({
+//         ok: true,
+//         json: () => Promise.resolve(mockResponse)
+//       });
+//     });
+
+//   it('should call fetch with the correct url', () => {
+//     fetchData(mockResponse);
+//     expect(window.fetch).toHaveBeenCalledWith('https://api.themoviedb.org/3/movie/now_playing?api_key=cd7eb6a4cff8273d777385057dcf9b56')
+//   })
+// })
+// })
