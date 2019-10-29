2307c0b628e08c069bb7269d54128e8ecbf85787
dawnlunacy <rudd.lacy@gmail.com>
2019-10-29 12:15:10 -0600

    Add second test to currentUser Reducer

--- src/reducers/currentUser.test.js
+++ src/reducers/currentUser.test.js
@@ -2,13 +2,26 @@ import { currentUser } from '../reducers/currentUser';
 
 describe('currentUser', () => {
   it('should return the initial state', () => {
-    //Setup
     const expected = null;
 
-    //Execution
     const result = currentUser(undefined, {});
 
-    //Expecation
     expect(result).toEqual(expected);
+  });
+
+  it('should return state with a new currentUser', () => {
+    const mockCurrentUser = {id:9999, name: "Pumpkin", email:"Kitty@lovesHairTies.com"}
+    const initialState = null;
+    const state = initialState;
+    const action = {
+      type: "SAVE_USER",
+      currentUser: mockCurrentUser
+    }
+
+    const newState = mockCurrentUser;
+
+    const result = currentUser(state, action);
+
+    expect(result).toEqual(newState);
   });
 });
