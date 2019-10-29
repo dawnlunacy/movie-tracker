ac8880f1b6c1e4a6cb6f9d3e6ea6f8b92a9ddf04
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-28 22:48:08 -0600

    Show selected movie with styling

--- src/containers/Movie/Movie.js
+++ src/containers/Movie/Movie.js
@@ -32,16 +32,15 @@ const Movie = ({ movie_id, title, poster_path, release_date, vote_average, overv
         <img className="poster-img" alt="Movie Poster" src = {`https://image.tmdb.org/t/p/original/${poster_path}`}/>
       </div>
       <img src={starImg} alt="Logo" className="movie-star" onClick={() => handleClick()}/>
-      {/* <Link to={`movies/${movie_id}`}> */}
-        <div className="movie-text" onClick={() => selectMovie({movie_id})}>
+      <Link to={`movies/${movie_id}`}>
+        <div className="movie-text" onClick={() => selectMovie({
+          movie_id, title: title, poster_path, release_date, vote_average, overview})}>
           <h3 className="movie-h3">{title}</h3>
         </div>
-      {/* </Link> */}
+      </Link>
     </section>
   )
 }
-
-// selectMovie({movie_id, title, poster_path, release_date, vote_average, overview})
 
 export const mapStateToProps = state => ({
   currentUser: state.currentUser,
