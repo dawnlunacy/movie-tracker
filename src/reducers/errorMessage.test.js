99bf7ac5e94ff268a028ff7e4b2a9485870e8827
dawnlunacy <rudd.lacy@gmail.com>
2019-10-29 13:47:20 -0600

    Add test to errorMessage Reducer

--- src/reducers/errorMessage.test.js
+++ src/reducers/errorMessage.test.js
@@ -8,4 +8,17 @@ describe('errorMessage', () => {
 
     expect(result).toEqual(expected);
   });
+
+  it('should return an state with a new error', () => {
+    const initialState = '';
+    const state = initialState;
+    const action = {
+      type: "HANDLE_ERROR",
+      errorMessage: "Username or Password is incorrect"
+    }
+
+    const result = errorMessage(state, action)
+
+    expect(result).toEqual(action.errorMessage)
+  });
 });
