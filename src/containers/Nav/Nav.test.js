7601b40866c635bd2d0d10ac165a721dbdf6215a
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-29 11:07:29 -0600

    Update Nav test to incorporate a beforeEach and outline mapStateToProps test

--- src/containers/Nav/Nav.test.js
+++ src/containers/Nav/Nav.test.js
@@ -4,11 +4,35 @@ import { shallow } from 'enzyme';
 
 describe('Nav', () => {
 
-  it('should match snapshot', () => {
+  let wrapper;
+  let mockCurrentUser;
+  beforeEach(() => {
+    mockCurrentUser = {id:3, name: "Tyler", email:"awesome@gmail.com"}
     const getFavorites = jest.fn()
-    const mockCurrentUser = {id:3, name: "Tyler", email:"awesome@gmail.com"}
-    const wrapper = shallow(<Nav getFavorites={getFavorites} currentUser={mockCurrentUser}/>)
+    wrapper = shallow(<Nav getFavorites={getFavorites} currentUser={mockCurrentUser}/>)
+  });
+
+  it('should match snapshot', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  // describe('mapStateToProps', () => {
+  //   it('should return an object with the currentUser', () => {
+
+
+  //     const mockState = {
+  //       selectedMovie: [{selectedMovie: mockSelectedMovie}],
+  //       filter: 'SELECT_MOVIE'
+  //     };
+  //     const expected = {
+  //       selectedMovie: [{selectedMovie: mockSelectedMovie}]
+  //     };
+
+  //     const mappedProps = mapStateToProps(mockState);
+
+  //     expect(mappedProps).toEqual(expected);
+  //   });
+  // });
+
 });
