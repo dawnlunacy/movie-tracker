f6bc1419e513c69ef9c37cc2182018c6e1ce13cb
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-28 21:29:10 -0600

    Attempt to edit movie snapshot test but have issue with favorites

--- src/containers/Movie/Movie.test.js
+++ src/containers/Movie/Movie.test.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Movie from './Movie';
+import { Movie, mapStateToProps, favorited } from './Movie';
 
 describe('Movie', () => {
-    it('should render the movie data in <section> tag correctly', () => {
-        const wrapper = shallow(<Movie 
-            key = {420809}
-            id = {420809}
-            title = {"Joker"}
-            poster = {"/tBuabjEqxzoUBHfbyNbd8ulgy5j.jpg"}
-            rating = {8.6}
-            overview = {"During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure."}
-        />);
+    it('Movie should render the movie data in <section> tag correctly', () => {
 
-        expect(wrapper).toMatchSnapshot();
+      const mockToggleFavorite = jest.fn();
+      const mockToggleStar = jest.fn();
+      
+// this snapshot does not pass, what do do with mockFavorites?
+      const mockFavorites = [
+        {
+          id: 475557,
+          overview: "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
+          poster: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+          rating: 8.6,
+          title: "Joker"
+        },
+        {
+          id: 420809,
+          overview: "Maleficent and her goddaughter Aurora begin to question the complex family ties that bind them as they are pulled in different directions by impending nuptials, unexpected allies, and dark new forces at play.",
+          poster: "/tBuabjEqxzoUBHfbyNbd8ulgy5j.jpg",
+          rating: 7.1,
+          title: "Maleficent: Mistress of Evil"}
+      ]
+
+      const wrapper = shallow(<Movie
+        key = {420809}
+        movie_id = {420809}
+        title = {"Joker"}
+        poster_path = {"/tBuabjEqxzoUBHfbyNbd8ulgy5j.jpg"}
+        release_date = {2019-10-25}
+        vote_average = {8.6}
+        overview = {"During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure."}
+        toggleFavorite = {mockToggleFavorite}
+        toggleStar = {mockToggleStar}
+      />);
+
+      expect(wrapper).toMatchSnapshot();
     });
 });
