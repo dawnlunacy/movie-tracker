630f367e99af1960ded82d10a6d327b8896d416a
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-01 13:04:55 -0700

    Deploy to gh pages

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './reducers'
-import { BrowserRouter as Router} from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 
 const store = createStore(rootReducer, composeWithDevTools());
 
 const router = (
-  <Router>
-    <Provider store={store}>
+  <Provider store={store}>
+    <Router>
       <App />
-    </Provider>
-  </Router>
+    </Router>
+  </Provider>
 )
 ReactDOM.render(router, document.getElementById('root'));
